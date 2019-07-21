15f316ab297b5269e96f239fbd52a3cadd37fb55
sdyin <sdyin@foxmail.com>
2019-07-21 10:53:55 +0800

    微调

--- src/main/java/com/sdyin/design/decorator/Test.java
+++ src/main/java/com/sdyin/design/decorator/Test.java
@@ -2,6 +2,7 @@ package com.sdyin.design.decorator;
 
 /**
  * 测试装饰者模式
+ * 装饰者构造器 都注入接口功能 -》 逐步增强扩展
  * @Description:
  * @Author: liuye
  * @time: 2019/7/20$ 下午10:18$
--- src/main/java/com/sdyin/design/proxy/Test.java
+++ src/main/java/com/sdyin/design/proxy/Test.java
@@ -1,6 +1,8 @@
 package com.sdyin.design.proxy;
 
 /**
+ * 测试代理模式
+ * 代码：代理类构造器 注入真实对象，真实对象处理逻辑。只做前置 后置处，控制对象访问权
  * @Description:
  * @Author: liuye
  * @time: 2019/7/21$ 上午10:25$
