047cdf6ee881638e1b4a8c4c94f1783b808f62dd
liuye <liuye@xforceplus.com>
2020-06-18 11:50:44 +0800

    LockSupport demo 调整增加注释

--- src/main/java/com/sdyin/design/lock/LockSupportDemo.java
+++ src/main/java/com/sdyin/design/lock/LockSupportDemo.java
@@ -16,7 +16,11 @@ public class LockSupportDemo {
         // 10 * 2的3次方
         System.out.println(10 << 3);
         System.out.println(LocalDateTime.now() + ":主线程开始执行");
-        LockSupport.parkNanos(1000 * 1000 * 1000);
+        //参数1： blocker 非静态方法中建议传入this
+        //park的三种形式也支持blocker对象参数。 线程被阻止时记录此对象，以允许监视和诊断工具识别线程被阻止的原因。
+        // （此类工具可以使用方法getBlocker(Thread)访问阻止程序 。）强烈鼓励使用这些形式而不是没有此参数的原始形式。
+        // 在锁实现中作为blocker提供的正常参数是this 。
+        LockSupport.parkNanos(LockSupportDemo.class,1000 * 1000 * 1000);
         System.out.println(LocalDateTime.now() + ":主线程执行完毕");
 
         Thread t1 = new Thread(() ->{
